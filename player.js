bff132987ec36f80406bbedf34555b7c124ee854
Jeflux <jespermathiason@gmail.com>
2020-01-02 19:23:46 +0100

    Started working on feature for playing music locally using audio jack

--- player.js
+++ player.js
@@ -1,6 +1,7 @@
-const shuffleArray = require('shuffle-array');
-const ytdl = require('ytdl-core');
-var SC = require('node-soundcloud');
+const shuffleArray = require('shuffle-array')
+const ytdl = require('ytdl-core')
+var SC = require('node-soundcloud')
+const portAudio = require('naudiodon')
 
 module.exports = {
     isPlaying: false,
@@ -13,36 +14,39 @@ module.exports = {
     currentPlayInfo: undefined,
     discordClient: undefined,
     volume: 0.1,
+    audioOut: undefined,
 
-    play: function(channel, connection, playInfo, isRetry) {
-        if (playInfo === undefined)
-            return;
-        // if (this.isPlaying === true)
-        //     return;
+    playLocally: function(playInfo)
+    {
+        stream = ytdl(playInfo.name)
+        stream.pipe(audioOut)
+        audioOut.start()
 
-        this.nowplaying = playInfo.metadata;
-
-        this.currentPlayInfo = playInfo;
-
+        stream.on("end", () => {
+            var _this = this
+            setTimeout(function() {
+                console.log("trying to play next")
+                _this.play(_this.queue.shift())
+            }, 200);
+        })
+    },
+    playConnection: function(channel, connection, playInfo, isRetry)
+    {
         if (playInfo.stream === false) {
             this.dispatcher = connection.playFile(playInfo.name, { passes: 2 })
         }
         else if (playInfo.metadata.url.indexOf("youtube.com") > -1) {
-            var stream = undefined;
+            var stream = undefined
             if (playInfo.metadata.isLive === undefined || playInfo.metadata.isLive === false)
                 stream = ytdl(playInfo.name, { filter : 'audioonly', quality_label: "720p" })
 	        else
                 stream = ytdl(playInfo.name)
             
-            this.dispatcher = connection.playStream(stream, { passes: 2 });
-        }
-        else if (playInfo.metadata.url.indexOf("soundcloud.com") > -1) {
-
-            //this.dispatcher = connection.playStream(, { passes: 2 });
+            this.dispatcher = connection.playStream(stream, { passes: 2 })
         }
 
-        this.dispatcher.setVolume(this.volume);
-        this.isPlaying = true;
+        this.dispatcher.setVolume(this.volume)
+        this.isPlaying = true
         if (isRetry === false && playInfo.metadata !== undefined) {
             if (playInfo.metadata.isLive === true)
                 channel.send("Now streaming **" + playInfo.metadata.title + "**")
@@ -51,121 +55,145 @@ module.exports = {
         }
 
         if (playInfo.metadata !== undefined)
-            this.discordClient.user.setGame(playInfo.metadata.title);
+            this.discordClient.user.setGame(playInfo.metadata.title)
 
 
 	    this.dispatcher.on("end", end => {
-            var _this = this;
+            var _this = this
             setTimeout(function() {
-                _this.dispatcher.removeAllListeners("end");
-                _this.dispatcher.stream.destroy();
-                _this.dispatcher.destroy();
+                _this.dispatcher.removeAllListeners("end")
+                _this.dispatcher.stream.destroy()
+                _this.dispatcher.destroy()
 
-                _this.discordClient.user.setGame("nothing");
+                _this.discordClient.user.setGame("nothing")
 
-                _this.isPlaying = false;
+                _this.isPlaying = false
 
                 if (end === undefined) {
-                    console.log("Song is not done yet");
-                    _this.dispatcher.stream = undefined;
-                    _this.play(channel, connection, playInfo, true);
-                    return;
+                    console.log("Song is not done yet")
+                    _this.dispatcher.stream = undefined
+                    _this.play(channel, connection, playInfo, true)
+                    return
                 }
 
                 if (_this.queue.length > 0) {
-                    console.log("trying to play next");
-                    _this.play(channel, connection, _this.queue.shift(), false);
+                    console.log("trying to play next")
+                    _this.play(channel, connection, _this.queue.shift(), false)
                 }
-            }, 200);
-	    });
+            }, 200)
+	    })
     
 	    this.dispatcher.on('error', e => {
-	    	console.log("Dispatcher error callback");
-            console.log(e);	
-	    });
+	    	console.log("Dispatcher error callback")
+            console.log(e)	
+	    })
+    },
+    play: function(channel, connection, playInfo, isRetry) {
+        if (playInfo === undefined)
+            return
+        // if (this.isPlaying === true)
+        //     return
+
+        this.nowplaying = playInfo.metadata
+        this.currentPlayInfo = playInfo
+
+        if (require("./settings.js").playLocally)
+            this.playLocally(playInfo)
+        else
+            this.playConnection(channel, connection, playInfo, isRetry)
+
     },
     start: function(message, connection) {
         if (this.isPlaying === true) {
-            console.log("Already playing");
-            return;
+            console.log("Already playing")
+            return
         }
 
-        this.channel = message.channel;
-        this.connection = connection;
+        audioOut = new portAudio.AudioIO({
+            outOptions: {
+                channelCount: 2,
+                sampleFormat: portAudio.SampleFormat16Bit,
+                sampleRate: 48000,
+                deviceId: -1 // Use -1 or omit the deviceId to select the default device
+            }
+        });
 
-        var date = new Date();
-        this.lastEnd = date.getMilliseconds();
+        this.channel = message.channel
+        this.connection = connection
+
+        var date = new Date()
+        this.lastEnd = date.getMilliseconds()
 
         if (this.queue.length > 0)
-            this.play(message.channel, connection, this.queue.shift(), false);
+            this.play(message.channel, connection, this.queue.shift(), false)
     },
     enqueue: function(channel, playInfo, unshift = false) {
         if (playInfo.metadata instanceof Array) {
-            channel.send("Adding **" + playInfo.metadata.length + "** songs to queue");
+            channel.send("Adding **" + playInfo.metadata.length + "** songs to queue")
             playInfo.metadata.forEach(function(metadata) {
                 if (unshift)
-                    this.queue.unshift({ name: metadata.url, stream: playInfo.stream, metadata });
+                    this.queue.unshift({ name: metadata.url, stream: playInfo.stream, metadata })
                 else
-                    this.queue.push({ name: metadata.url, stream: playInfo.stream, metadata });
-            }, this);
+                    this.queue.push({ name: metadata.url, stream: playInfo.stream, metadata })
+            }, this)
         }
         else {
             if (playInfo.metadata !== undefined)
-                channel.send("Adding **" + playInfo.metadata.title + "** to queue");
+                channel.send("Adding **" + playInfo.metadata.title + "** to queue")
 
             if (unshift)
-                this.queue.unshift(playInfo);
+                this.queue.unshift(playInfo)
             else
-                this.queue.push(playInfo);
+                this.queue.push(playInfo)
         }
     },
     pause: function() {
         if (this.dispatcher !== undefined)
-            this.dispatcher.pause();
+            this.dispatcher.pause()
     },
     resume: function() {
         if (this.dispatcher !== undefined)
-            this.dispatcher.resume();
+            this.dispatcher.resume()
     },
     skip: function() {
-        this.discordClient.user.setGame("nothing");
+        this.discordClient.user.setGame("nothing")
         
-        this.isPlaying = false;
+        this.isPlaying = false
         if (this.dispatcher !== undefined) {
-            this.dispatcher.removeAllListeners("end");
-            this.dispatcher.on("end", end => { });
-            this.dispatcher.end("skip");
-            this.dispatcher.stream.destroy();
-            this.dispatcher.destroy();
-            this.play(this.channel, this.connection, this.queue.shift(), false);    
+            this.dispatcher.removeAllListeners("end")
+            this.dispatcher.on("end", end => { })
+            this.dispatcher.end("skip")
+            this.dispatcher.stream.destroy()
+            this.dispatcher.destroy()
+            this.play(this.channel, this.connection, this.queue.shift(), false)    
         }
     },
     getQueue: function() {
-        return this.queue;
+        return this.queue
     },
     getNowPlaying: function() {
-        return this.nowplaying;
+        return this.nowplaying
     },
     destroy: function() {
         if (this.dispatcher === undefined)
-            return;
-        this.discordClient.user.setGame("nothing");
-        this.dispatcher.removeAllListeners("end");
-        this.dispatcher.on("end", end => {});
-        this.dispatcher.end();
+            return
+        this.discordClient.user.setGame("nothing")
+        this.dispatcher.removeAllListeners("end")
+        this.dispatcher.on("end", end => {})
+        this.dispatcher.end()
     },
     remove: function(begin, end) {
         if (end === undefined) {
-            this.queue.splice(begin, this.queue.length - begin);
-            return;
+            this.queue.splice(begin, this.queue.length - begin)
+            return
         }
         // 0-indexed
-        begin--;
-        end--;
-        var numToRemove = end - begin + 1;
-        console.log(this.queue.splice(begin, numToRemove));
+        begin--
+        end--
+        var numToRemove = end - begin + 1
+        console.log(this.queue.splice(begin, numToRemove))
     },
     shuffle: function() {
-        shuffleArray(this.queue);
+        shuffleArray(this.queue)
     }
 }
